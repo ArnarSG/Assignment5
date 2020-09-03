bc9a5b74ef5aaaa4637c02364b9f75552ede04a0
Arnar Sveinn Gudmundsson <arnarsveinngudmundsson@Arnars-MacBook-Pro-4.local>
2020-09-03 14:43:17 +0000

    added code that was missing

--- sequence.py
+++ sequence.py
@@ -1,5 +1,22 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-# add a loop that calculates (n-2)+(n-1)+n
-#print out sequence
+first_n = 3
+second_n = 2
+third_n = 1
+counter = 0
+the_sum = 0
+while counter < n:
+    if counter < 3:
+        the_sum += 1
+        counter += 1
+        print(the_sum)
+    else:
+        the_sum = first_n + second_n + third_n
+        third_n = second_n
+        second_n = first_n
+        first_n = the_sum
+        counter += 1
+        print(the_sum)
+        
+    
 
     
